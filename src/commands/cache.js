4768f1f290b22ad1311e91a35a426159ccae8b74
Donald Pakkies <donaldpakkies@gmail.com>
2021-09-07 14:48:52 +0200

    feat: run cache command twice
    this is meant to be a temp fix

--- src/commands/cache.js
+++ src/commands/cache.js
@@ -8,40 +8,51 @@ class CacheCommand extends Command {
   async run() {
     const { flags } = this.parse(CacheCommand)
 
-    cli.action.start('Caching')
+    cli.action.start('Caching');
 
-    const output = path.join(process.cwd(), 'node_modules', '.cache', 'formidable')
-
-    const command = `node node_modules/.bin/imba build server.cli.imba --outdir=${output} --clean`
-
-    const imba = exec(command)
-
-    imba.stdout.on('data', async (data) => {
-      if (data.includes('finished in ')) {
-        const { Application } = require(path.join(output,'server.cli.js'))
-
-        await Application.then((app) => {
-          fs.rmSync(output, { recursive: true });
-
-          cli.action.stop()
-        });
-      }
-    });
-
-    imba.stderr.on('data', (data) => {
-      if (flags.debug) {
-        return console.error(data.trim());
-      }
-
-      cli.action.stop('failed')
-    });
-
-    imba.on('exit', () => {
-      process.exit(0)
-    })
+    cache(1, flags.debug);
   }
 }
 
+const cache = (tick = 1, debug) => {
+  const output = path.join(process.cwd(), 'node_modules', '.cache', 'formidable')
+
+  const command = `node node_modules/.bin/imba build server.cli.imba --outdir=${output} --clean`
+
+  const imba = exec(command)
+
+  imba.stdout.on('data', async (data) => {
+    if (data.includes('finished in ')) {
+      const { Application } = require(path.join(output,'server.cli.js'))
+
+      await Application.then((app) => {
+        fs.rmSync(output, { recursive: true });
+
+        if (tick === 1) {
+          cache(2, debug);
+
+          return;
+        }
+
+        cli.action.stop();
+      });
+    }
+  });
+
+  imba.stderr.on('data', (data) => {
+    if (debug) {
+      console.error(data.trim());
+
+      return console.log('Run with --debug to see the detailed error.')
+    }
+
+    cli.action.stop('failed');
+  });
+
+  imba.on('exit', () => {
+    process.exit(0)
+  });
+}
 CacheCommand.description = `Cache application config`
 
 CacheCommand.flags = {
