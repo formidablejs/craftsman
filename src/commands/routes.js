08876af516995c18798632e6bdf67dd5dd7af831
Donald Pakkies <donaldpakkies@gmail.com>
2021-09-15 21:45:30 +0200

    feat: change entry file

--- src/commands/routes.js
+++ src/commands/routes.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 class RoutesCommand extends Command {
   async run() {
-    const { Application } = require(path.join(process.cwd(), 'bootstrap', 'compiled', 'server.cli.js'));
+    const { Application } = require(path.join(process.cwd(), 'dist', 'server.app.js'));
 
     await Application.then((app) => {
       console.table(app.routes());
