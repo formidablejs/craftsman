5fd400ba7d7e9bbef9d1aee4ff1172c6f849cb9b
Donald Pakkies <donaldpakkies@gmail.com>
2021-09-16 19:17:45 +0200

    feat: add seeder command
    add support for knex seeder

---
+++ src/commands/seed.js
@@ -0,0 +1,36 @@
+const { Command } = require('@oclif/command');
+const { default: chalk } = require('chalk');
+const path = require('path');
+
+class SeedCommand extends Command {
+
+  async run() {
+    const { Application } = require(path.join(process.cwd(), 'dist', 'server.app.js'));
+
+    await Application.then(async (app) => {
+      console.log('Using environment: ' + chalk.cyan(app.config.get('app.env')));
+
+      const results = await app.seeder().run();
+
+      if (results == false) {
+        throw new Error('Seeding failed');
+      }
+
+      if (Array.isArray(results)) {
+        results[0].forEach((seeder) => {
+          console.log(chalk.green('Seeded: ') + seeder);
+        });
+
+        return;
+      }
+
+      console.error(results);
+    });
+
+    this.exit();
+  }
+}
+
+SeedCommand.description = 'Run seeders';
+
+module.exports = SeedCommand;
